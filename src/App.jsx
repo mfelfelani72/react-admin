39cd233855767d073b289de35ad01db0f0ac1d0c
mohammad <mfelfelani72@gmail.com>
2025-08-01 08:19:27 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <div className='h-96 w-96 bg-rose-500'>sdfds</div>
+    </>
+  )
+}
+
+export default App
