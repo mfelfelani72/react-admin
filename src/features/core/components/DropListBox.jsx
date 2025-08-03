cc907394788dd446585dd9ef3b7cc2a8138141d4
Yasin <ton.618@yahoo.com>
2025-08-03 20:57:06 +0330

    update sidebar

--- src/features/core/components/DropListBox.jsx
+++ src/features/core/components/DropListBox.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
+// Components
+
+import { AngleIcon } from "./Icon.jsx";
+
 // Functions
 
 import { cn } from "../../../utils/libs/cn";
+
 const DropListBox = ({ className, ...props }) => {
   return (
     <>
       <div
         className={cn(
-          "flex flex-row justify-between hover:bg-blue-100 rounded-lg cursor-ponter select-none px-4 py-2 hover:text-blue-500 transition-all ease-in-out",
+          "flex flex-row justify-between hover:bg-blue-100 rounded-lg cursor-pointer select-none px-4 py-2 hover:text-blue-500 transition-all ease-in-out",
           className
         )}
       >
@@ -16,7 +21,7 @@ const DropListBox = ({ className, ...props }) => {
           <div>{props?.icon}</div>
           <div>{props?.title}</div>
         </div>
-        <div>arrow</div>
+        <div><AngleIcon /></div>
       </div>
     </>
   );
