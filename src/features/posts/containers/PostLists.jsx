618e36a05d2afaaec808ec9d570c6bcc1e9d52e3
mohammad <mfelfelani72@gmail.com>
2025-08-17 13:39:25 +0330

    update

--- src/features/posts/containers/PostLists.jsx
+++ src/features/posts/containers/PostLists.jsx
@@ -13,10 +13,10 @@ const PostLists = () => {
       age: 28,
       job: "توسعه‌دهنده",
       city: "تهران",
-      // country: "ایران",
-      // score: 95,
-      // status: "فعال",
-      // date: "1402/05/15",
+      country: "ایران",
+      score: 95,
+      status: "فعال",
+      date: "1402/05/15",
     },
     {
       id: 2,
@@ -24,10 +24,10 @@ const PostLists = () => {
       age: 32,
       job: "طراح",
       city: "مشهد",
-      // country: "ایران",
-      // score: 88,
-      // status: "غیرفعال",
-      // date: "1402/04/22",
+      country: "ایران",
+      score: 88,
+      status: "غیرفعال",
+      date: "1402/04/22",
     },
     {
       id: 3,
@@ -35,10 +35,10 @@ const PostLists = () => {
       age: 45,
       job: "مدیر",
       city: "نیویورک",
-      // country: "آمریکا",
-      // score: 76,
-      // status: "فعال",
-      // date: "1401/12/03",
+      country: "آمریکا",
+      score: 76,
+      status: "فعال",
+      date: "1401/12/03",
     },
   ];
   return (
@@ -46,62 +46,14 @@ const PostLists = () => {
       <div className="relative z-5">
         <Table
           data={data}
+          customColumnConfig={{
+            id: { title: "id", sortable: false,  baseWidth: 50 },
+          }}
           onDelete={(id) => console.log("Delete item with id:", id)}
           onView={(item) => console.log("View item:", item)}
           onEdit={(item) => console.log("Edit item:", item)}
         />
       </div>
-      {/* <div className="min-h-[calc(100vh-5.8rem)] flex flex-col overflow-hidden rounded-t-2xl">
-        <table className="flex-1 overflow-y-auto bg-white rounded-2xl">
-          <colgroup>
-            <col className="width-[52px]" />
-            <col className="width-[323px]" />
-            <col className="width-[282px]" />
-            <col className="width-[173px]" />
-            <col className="width-[317px]" />
-            <col className="width-[216px]" />
-            <col className="width-[240px]" />
-          </colgroup>
-
-          <thead className="border-b h-[5rem]">
-            <tr className="text-left">
-              <th className="pl-[1.5rem]">
-                <input type="checkbox" />
-              </th>
-              <TableCell title={"Students"} icon={<DashboardIcon />} />
-              <TableCell title={"Email ID"} icon={<DashboardIcon />} />
-              <TableCell title={"Courses"} icon={<DashboardIcon />} />
-              <TableCell title={"Certificates Earned"} icon={<DashboardIcon />} />
-              <TableCell title={"Status"} icon={<DashboardIcon />} />
-              <TableCell title={"Actions"} />
-            </tr>
-          </thead>
-          <tbody>
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-            <TableRow name={"Yasin Amini"} email={"ton.618@yahoo.com"} cours={"3"} certificate={"2"} />
-          </tbody>
-        </table>
-        <Footer />
-      </div> */}
     </>
   );
 };
